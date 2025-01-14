54c60d85912af2218012394c9b5d5438ece84ed5
DongSuLee <101309717+dongsu0717@users.noreply.github.com>
2025-01-14 18:31:31 +0900

    [Add]: 세팅 build-logic Module

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,3 +1,4 @@
+/*
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
@@ -6,4 +7,35 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}*/
+
+/*
+plugins {
+    `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
 }
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+//    implementation(libs.verify.detektPlugin)
+//    compileOnly(libs.compose.compiler.gradle.plugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidHilt") {
+            id = "dongsu.android.hilt"
+            implementationClass = "com.dongsu.newstalk.HiltAndroidPlugin"
+        }
+        register("kotlinHilt") {
+            id = "dongsu.kotlin.hilt"
+            implementationClass = "com.dongsu.newstalk.HiltKotlinPlugin"
+        }
+        register("androidRoom") {
+            id = "dongsu.android.room"
+            implementationClass = "com.dongsu.newstalk.AndroidRoomPlugin"
+        }
+    }
+}
+*/
